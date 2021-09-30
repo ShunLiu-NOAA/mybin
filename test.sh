3084d6c2fe510fe046e6b88fa631bdb927adbb8f
Shun Liu <Shun.Liu@clogin01.cactus.wcoss2.ncep.noaa.gov>
2021-09-30 15:34:26 -0400

    test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 #set -x
+#############
+# extract substring
+#  ${string:P:L}
+#Here P is a number that indicates the starting index of the substring and L is the length of the substring. 
+#If you omit the L parameter then the rest of the string is returned, starting from position P
+#############
+mydate=2021082700
+echo ${mydate:0:8}
+exit
 
 ##############
 # test array number
