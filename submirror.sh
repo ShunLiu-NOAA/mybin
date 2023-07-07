c1253745238e79e58a9ce4f032bccfde6ee450a0
Shun Liu <Shun.Liu@noaa.gov>
2023-07-07 15:13:41 +0000

    submirror.sh

---
+++ submirror.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+flnm=$1 
+
+thisdir=`pwd`
+cd $thisdir
+
+rm -f /lfs/h2/emc/ptmp/emc.lam/mirror/rrfs_mirror.out
+cp $dnbns/bin/mirror.sh mirror1.sh
+target=$thisdir/$flnm
+sed -i "s|TARGET|${target}|g" mirror1.sh
+
+qsub mirror1.sh
