f757d60331414f9ee437360a38d9f9af77fda397
Shun Liu <Shun.Liu@noaa.gov>
2023-10-11 16:39:57 +0000

    submirror.sh

--- submirror.sh
+++ submirror.sh
@@ -6,6 +6,7 @@ thisdir=`pwd`
 cd $thisdir
 
 rm -f /lfs/h2/emc/ptmp/emc.lam/mirror/rrfs_mirror.out
+mkdir -p /lfs/h2/emc/ptmp/emc.lam/mirror
 cp $dnbns/bin/mirror.sh mirror1.sh
 target=$thisdir/$flnm
 sed -i "s|TARGET|${target}|g" mirror1.sh
