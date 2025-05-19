6dfb3aa6b66a93866d4e18f34ef8fb03bfd6f99e
Shun Liu <Shun.Liu@noaa.gov>
2025-05-19 17:20:52 +0000

    check_job.sh

--- check_job.sh
+++ check_job.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 curtime=$(date +"%Y%m%d.%H%M")
+curhour=$(date +"%Y%m%d.%H")
 curdate=$(date +"%Y%m%d")
+
+#curdatem1=`date "+%Y%m%d%H" --date="1 hour ago"`
+#curdatem2=`date "+%Y%m%d%H" --date="2 hour ago"`
+#curdatem3=`date "+%Y%m%d%H" --date="3 hour ago"`
+
+curdatem1=$(date -d '1 hour ago' +"%H")
+curdatem2=$(date -d '2 hour ago' +"%H")
+curdatem3=$(date -d '3 hour ago' +"%H")
+
+echo $curdatem1, $curdatem2, $curdatem3
 
 logdir=/lfs/h2/emc/ptmp/emc.lam/rrfs/v0.9.1/logs/enkfrrfs.$curdate
 cd $logdir
 
-allcyc=$(ls -1d * | grep -Eo '^[0-9]+')
+#allcyc=$(ls -1d * | grep -Eo '^[0-9]+')
+allcyc='$curdatem1 $curdatem2 $curdatem3'
 echo $allcyc
 
 
@@ -18,7 +30,8 @@ cd $cyc
 logfile=FV3LAM_wflow_n3enkf91.log
 
 #str1=$(grep 'Submission of cldanl_nonvar_prod_mem0001 succeeded' "$logfile")
-str1=$(grep 'Submitting cldanl_nonvar_prod_mem0001' "$logfile")
+#str1=$(grep 'Submitting cldanl_nonvar_prod_mem0001' "$logfile")
+str1=$(grep 'Submitting enkf_radarref' "$logfile")
 str2=$(grep 'run_fcst_prod_mem0001, ' "$logfile" | grep 'SUCCEEDED (F)')
 str3=$(grep 'run_fcst_prod_long_mem0001, ' "$logfile" | grep 'SUCCEEDED (F)')
 
@@ -44,7 +57,8 @@ if [ -n "$str1" ]; then
 fi
 
 # Output results
-echo "str1: $str1"
+#jobstatus=0 is no block, jobstatus=1 is block submission
+echo "str1: $str1", $starttime, $endtime
 echo "str2: $str2"
 echo "str3: $str3"
 echo "Job Status: $jobstatus"
