d50678cd6b5cebc083233f98bd1a2fb7bd923738
Shun Liu <Shun.Liu@noaa.gov>
2025-05-19 17:23:59 +0000

    find_between_time.sh

--- find_between_time.sh
+++ find_between_time.sh
@@ -1,9 +1,32 @@
 
-cd /lfs/h2/emc/ptmp/emc.lam/rrfs/v0.9.1/logs/rrfs.20250508
+mydate=20250516
+stime=1805
+etime=1815
+cd /lfs/h2/emc/ptmp/emc.lam/rrfs/v0.9.1/logs/rrfs.$mydate
 # Step 1: Create reference timestamp files
-touch -t 202505081800 start.ref
-touch -t 202505081815 end.ref
+touch -t ${mydate}${stime} start.ref
+touch -t ${mydate}${etime} end.ref
 
+echo ========================================
+# Step 2: Use find to locate files modified between those times
+find . -type f -newer start.ref ! -newer end.ref -exec stat --format='%y %n' {} \;
+echo ========================================
+
+cd /lfs/h2/emc/ptmp/emc.lam/rrfs/v0.9.1/logs/enkfrrfs.$mydate
+# Step 1: Create reference timestamp files
+touch -t ${mydate}${stime} start.ref
+touch -t ${mydate}${etime} end.ref
+
+echo ========================================
+# Step 2: Use find to locate files modified between those times
+find . -type f -newer start.ref ! -newer end.ref -exec stat --format='%y %n' {} \;
+
+cd /lfs/h2/emc/ptmp/emc.lam/rrfs/v0.9.1/logs/refs.$mydate
+# Step 1: Create reference timestamp files
+touch -t ${mydate}${stime} start.ref
+touch -t ${mydate}${etime} end.ref
+
+echo ========================================
 # Step 2: Use find to locate files modified between those times
 find . -type f -newer start.ref ! -newer end.ref -exec stat --format='%y %n' {} \;
 
