465b7345763b6deb34d2fa5f92404f2981b5a9c8
Shun Liu <Shun.Liu@noaa.gov>
2025-10-22 14:50:32 +0000

    subhpss.sh

---
+++ subhpss.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+module load envvar/1.0
+module load prod_util/2.0.10
+
+thisdate=`$MDATE`
+echo $thisdate
+
+flnm=$1
+
+thisdir=`pwd`
+cd $thisdir
+
+#rm -f /lfs/h2/emc/ptmp/emc.lam/mirror/rrfs_mirror.out
+mkdir -p  /lfs/h2/emc/ptmp/emc.lam/mirror
+mkdir -p /lfs/h2/emc/ptmp/emc.lam/Shun.Liu
+#cp $dnbns/bin/mirror.sh mirror1.sh
+cp htar_sub_orig.sh htar_sub.sh
+#target=$thisdir/$flnm
+target=$flnm
+sed -i "s|TARGET|${target}|g" htar_sub.sh
+sed -i "s|THISDATE|${thisdate}|g" htar_sub.sh
+sed -i "s|THISDIR|${thisdir}|g" htar_sub.sh
+
+qsub htar_sub.sh
+
+exit
