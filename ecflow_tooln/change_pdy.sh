b2bb0904443c36d41f7567845a169f5658921f70
Shun Liu <Shun.Liu@noaa.gov>
2026-04-26 13:44:04 +0000

    change_pdy.sh

--- ecflow_tooln/change_pdy.sh
+++ ecflow_tooln/change_pdy.sh
@@ -21,8 +21,8 @@ for cyc in $(seq -w 00 11); do
 g_cyc_tmp=$(( (10#$cyc / 6) * 6 ))
 g_cyc=$(printf "%02d" $g_cyc_tmp)
 #ecflow_client --host $ECF_HOST --port $ECF_PORT --alter add variable ECF_INCLUDE /lfs/h2/emc/lam/noscrub/emc.lam/rrfs/ecflow/rrfs-workflow/ecf/include /emc_rrfs_dev/primary/${g_cyc}
-ecflow_client --host $ECF_HOST --port $ECF_PORT --alter add variable PDY 20260410 /para/primary/${g_cyc}/rrfs/v1.0/${cyc}z
-ecflow_client --host $ECF_HOST --port $ECF_PORT --alter add variable PDY 20260410 /para/primary/${g_cyc}/rrfs/v1.0/cycle_end
+ecflow_client --host $ECF_HOST --port $ECF_PORT --alter add variable PDY 20260425 /para/primary/${g_cyc}/rrfs/v1.0/${cyc}z
+ecflow_client --host $ECF_HOST --port $ECF_PORT --alter add variable PDY 20260425 /para/primary/${g_cyc}/rrfs/v1.0/cycle_end
 #ecflow_client --host $ECF_HOST --port $ECF_PORT --alter add variable PDY 20250804 /emc_rrfs_dev_${cyc}/primary/${cyc}
 #ecflow_client --host $ECF_HOST --port $ECF_PORT --alter add variable QUEUE devhigh /emc_rrfs_dev/primary/${cyc}
 #ecflow_client --host $ECF_HOST --port $ECF_PORT --alter add variable MACHINE_SITE development /emc_rrfs_dev}/primary/${cyc}
